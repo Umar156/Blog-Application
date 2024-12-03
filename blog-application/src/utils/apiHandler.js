81a4aedd51c1206af9b111979925888387542c86
Umar <omarhafeez786@gmail.com>
2024-12-03 14:33:52 +0500

    change

--- blog-application/src/utils/apiHandler.js
+++ blog-application/src/utils/apiHandler.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://blog-application-uvgy.vercel.app";
+axios.defaults.baseURL = "https://blog-application-gbcu.vercel.app";
 
 const customAxios = (endpoint, method, data = null, header = null) => {
   return new Promise((resolve, reject) => {
